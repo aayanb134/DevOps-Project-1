5ea3c561caa1141d4207b78722b5cf11c50060be
AayanButt <aayanb134@gmail.com>
2023-11-22 21:45:47 +0000

    finished main.tf - need to add vars

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,3 +1,40 @@
-resource "aws_ecr_repository" "API-Repo" {
-  name = "API Repo"
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_ecs_cluster" "my-cluster" {
+  name = "my-ecs-cluster"
+}
+
+resource "aws_ecs_task_definition" "my-task-definition" {
+  family = "my-task-family"
+  network_mode = "bridge"
+  requires_compatibilities = ["FARGATE"]
+  cpu = 
+  memory = 
+
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+      "name": 
+      "image":
+      "cpu":
+      "memory":
+      "essential":
+    }
+  ]
+  TASK_DEFINITION
+}
+
+resource "aws_ecs_service" "my-ecs-service" {
+  name = ""
+  cluster = 
+  task_definition = aws_ecs_task_definition.my-task-definition.arn
+  launch_type = "FARGATE"
+  desired_count = 1
+
+  network_configuration {
+    subnets = [""] #put your subnet ID here
+    security_groups = [""] #put your SG ID here
+  }
 }
