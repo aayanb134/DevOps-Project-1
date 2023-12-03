59808522470b5a8a76ab7d7c06307b20d9697ec1
AayanButt <aayanb134@gmail.com>
2023-12-03 15:24:38 +0000

    added parameters

--- IaC/main.tf
+++ IaC/main.tf
@@ -7,34 +7,34 @@ resource "aws_ecs_cluster" "my-cluster" {
 }
 
 resource "aws_ecs_task_definition" "my-task-definition" {
-  family = "my-task-family"
-  network_mode = "bridge"
+  family                   = "my-task-family"
+  network_mode             = "bridge"
   requires_compatibilities = ["FARGATE"]
-  cpu = 
-  memory = 
+  cpu                      = 10
+  memory                   = 512
 
   container_definitions = <<TASK_DEFINITION
   [
     {
-      "name": ,
-      "image": ,
-      "cpu": ,
-      "memory": ,
-      "essential":
+      "name": "abcde",
+      "image": "<your-account-id>.dkr.ecr.<your-region>.amazonaws.com/your-repo-name:latest",
+      "cpu": 10,
+      "memory": 512 ,
+      "essential": true,
     }
   ]
   TASK_DEFINITION
 }
 
 resource "aws_ecs_service" "my-ecs-service" {
-  name = ""
-  cluster = 
+  name            = "name"
+  cluster         = aws_ecs_cluster.my-cluster.id
   task_definition = aws_ecs_task_definition.my-task-definition.arn
-  launch_type = "FARGATE"
-  desired_count = 1
+  launch_type     = "FARGATE"
+  desired_count   = 1
 
   network_configuration {
-    subnets = [""] #put your subnet ID here
+    subnets         = [""] #put your subnet ID here
     security_groups = [""] #put your SG ID here
   }
 }
